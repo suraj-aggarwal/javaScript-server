36d40455c4532f47ae89f72605ff3b5b6e081ee9
Suraj Aggarwal <suraj.aggarwal@lapr243.successivetech.com>
2020-01-31 21:09:12 +0530

    feat : add Server.ts and set .env

---
+++ src/Server.ts
@@ -0,0 +1,35 @@
+import * as express from 'express';
+
+class Server {
+    private app: express.Application;
+    constructor(private config) {
+        this.app = express();
+    }
+
+    bootstrap = () => {
+        this.setupRoutes();
+        return this;
+    }
+
+    run = () => {
+        const { app, config: { PORT } } = this;
+        app.listen(PORT, (err) => {
+            if (err) {
+                console.log('error');
+            }
+            console.log('sucessfull');
+        });
+        return this;
+    }
+
+    setupRoutes = () => {
+        const { app } = this;
+        app.get('/health-check', (req, res) => {
+            console.log('I am OK');
+            res.send('I am ok.');
+        });
+        return this;
+    }
+}
+
+export default Server;
