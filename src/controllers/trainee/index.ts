3bf1f667731645edc05c32258b24fc7a62d3e8fe
Suraj Aggarwal <suraj.aggarwal@lapr243.successivetech.com>
2020-02-13 20:47:06 +0530

    feat: add validations

--- src/controllers/trainee/index.ts
+++ src/controllers/trainee/index.ts
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
 export { default as traineeRoutes } from './routes';
 export { default as traineeValidation } from './validation';
-=======
-export { default as traineeRoutes } from './routes';
->>>>>>> 2d22f91d586db6234a68fe1dc7957bf7a766f134
--- src/controllers/trainee/routes.ts
+++ src/controllers/trainee/routes.ts
@@ -1,6 +1,5 @@
 import Controller from './Controller';
 import { Router } from 'express';
-<<<<<<< HEAD
 import validate from './validation';
 import validateTrainee from '../../libs/routes/validationHandler';
 
@@ -10,14 +9,5 @@ traineeRoute.get('/', validateTrainee(validate.get), Controller.listTrainee)
     .post('/', validateTrainee(validate.create), Controller.addTrainee)
     .put('/', validateTrainee(validate.update), Controller.updateTrainee)
     .delete('/:id', validateTrainee(validate.delete), Controller.deleteTrainee);
-=======
-
-const traineeRoute = Router();
-
-traineeRoute.get('/', Controller.listTrainee)
-    .post('/', Controller.addTrainee)
-    .put('/', Controller.updateTrainee)
-    .delete('/', Controller.deleteTrainee);
->>>>>>> 2d22f91d586db6234a68fe1dc7957bf7a766f134
 
 export default traineeRoute;
