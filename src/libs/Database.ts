64d4e5b4d3cee247bd097b7beed04b8b9fc7db63
Suraj Aggarwal <suraj.aggarwal@lapr243.successivetech.com>
2020-02-20 11:55:56 +0530

    feat: add Database.ts

--- src/libs/Database.ts
+++ src/libs/Database.ts
@@ -3,10 +3,12 @@ import * as mongoose from 'mongoose';
 class Database {
 
     static open = (uri: string) => new Promise( (resolve, reject)  => {
-        mongoose.connect(uri, { useNewUrlParser: true , useUnifiedTopology: true }, (err) => { 
+        mongoose.connect(uri, { useNewUrlParser: true , useUnifiedTopology: true }, (err) => {
             if (err) {
-                reject('connnection failed');
+                console.log('database connection failed.');
+                reject('database connnection failed');
             } else {
+                console.log('database connection successfull.');
                 resolve('connection is successfull');
             }
         });
