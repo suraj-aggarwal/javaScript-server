a1e32b51417523c8a5ac97103178438708546221
Suraj Aggarwal <suraj.aggarwal@lapr243.successivetech.com>
2020-02-24 18:04:57 +0530

    feat: add SystemResponse.ts

--- src/libs/routes/authMiddlerWare.ts
+++ src/libs/routes/authMiddlerWare.ts
@@ -2,17 +2,20 @@ import { Request, Response, NextFunction } from 'express';
 import configuration from '../../config/configuration';
 import * as jwt from 'jsonwebtoken';
 import hasPermission from '../utils/permissions';
+import UserRepository from '../../repositories/user/UserRepository';
 
 const authMiddlerWare = (module: string, permission: string) => (req: Request, res: Response, next: NextFunction) => {
     console.log('----------------------AUTHMIDDLE WARE------------------');
     try {
         const token: string = req.headers.authorization;
         const decodedPayload: any = jwt.verify(token, configuration.SECRET_KEY);
+        const {id, email} = decodedPayload;
+        const exits = isExits(id, email);
         if (!decodedPayload) {
             res.send(
-            {   error : 'Unatuhorized Acess.',
+            {   error : 'Unauthorized Access.',
                 Stauts: 401,
-                message: 'Unatuhorized Acess.'
+                message: 'Unauthorized Access.'
         });
         }
 
@@ -29,6 +32,12 @@ const authMiddlerWare = (module: string, permission: string) => (req: Request, r
     } catch (err) {
         throw err;
     }
+};
+
+const isExits = (id: string, email: string) => {
+    console.log('----------------Is Exits----------------');
+    const userRepo: UserRepository = new UserRepository();
+    return userRepo.isExits(id, email);
 };
 
 export default authMiddlerWare;
